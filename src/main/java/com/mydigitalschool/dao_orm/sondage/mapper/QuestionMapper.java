1fda30b86d60f6a5f73a7e45a8709f0f0078aff6
ofuro88 <guiffan.florestan@gmail.com>
2018-01-15 13:12:18 +0100

    déplacement du package Mapper

--- src/main/java/com/mydigitalschool/dao_orm/sondage/mapper/QuestionMapper.java
+++ src/main/java/com/mydigitalschool/dao_orm/sondage/mapper/QuestionMapper.java
@@ -1,18 +1,19 @@
 package com.mydigitalschool.dao_orm.sondage.mapper;
 
-import com.mydigitalschool.dao_orm.sondage.dtos.Question;
-import org.flywaydb.core.internal.util.jdbc.RowMapper;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class QuestionMapper implements RowMapper<Question> {
-    @Override
-    public Question mapRow(ResultSet resultSet) throws SQLException {
-        Question question = new Question();
-        question.setId(resultSet.getInt("id"));
-        question.setIntitule(resultSet.getString("intitule"));
+import org.flywaydb.core.internal.util.jdbc.RowMapper;
 
-        return question;
-    }
+import com.mydigitalschool.dao_orm.sondage.dtos.Question;
+
+public class QuestionMapper implements RowMapper<Question>{
+		
+		@Override
+		public Question mapRow(ResultSet rs) throws SQLException {
+			Question question = new Question();
+			question.setId(rs.getInt("id"));
+			question.setIntitule(rs.getString("name"));
+			return question;
+		}
 }
