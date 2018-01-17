5ba91f8dadbb412b67353d30d211f284d06bec32
ofuro88 <guiffan.florestan@gmail.com>
2018-01-17 18:21:54 +0100

    ajout des deux questions de l'applications (affichage console) ---manque un thread

--- src/main/java/com/mydigitalschool/dao_orm/sondage/Application.java
+++ src/main/java/com/mydigitalschool/dao_orm/sondage/Application.java
@@ -1,12 +1,27 @@
 package com.mydigitalschool.dao_orm.sondage;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
+import com.mydigitalschool.dao_orm.sondage.controllers.QueryController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.Console;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+//		// creation du controleur
+//		QueryController queryController = new QueryController();
+//
+//		// lancement des requetes affichées en log
+//			// nombre de participants
+//		System.out.print("combien y a-t-il des participants au sondage ?");
+//		System.out.println(queryController.participants().size());
+//			// nombre questions ?
+//		System.out.print("combien y a-t-il des questions dans le sondage ?");
+//		System.out.println(queryController.questions().size());
 	}
 }
